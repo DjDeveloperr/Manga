53915de9103a5b8c3ea9c8dc4dc8e1382d0ad9dd
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2021-09-20 11:59:51 +0530

    add proxyURL to pages

--- src/chapter.ts
+++ src/chapter.ts
@@ -24,6 +24,7 @@ export async function scrapeChapter(
           .replaceAll("- MangaNato.com", "")
           .trim(),
         url: e.attribs.src,
+        proxyURL: `https://manga-proxy.deno.dev/?q=${encodeURIComponent(e.attribs.src)}`,
       });
     }
   });
