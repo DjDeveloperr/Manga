629c09e7e5cd58684da64de07b55e64bfae4738b
Alexiy Rybin <0xtapnisu@gmail.com>
2022-10-23 17:29:08 +0700

    Fixed search

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/sift@0.3.0/mod.ts";
-export * from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+export * from "https://deno.land/x/cheerio@1.0.7/mod.ts";
+export * from "https://deno.land/x/sift@0.6.0/mod.ts";
