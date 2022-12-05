5a6c8d42b5511305b8f1f4158eddd676ec2751f4
Dj <43033058+DjDeveloperr@users.noreply.github.com>
2022-12-05 17:11:27 +0530

    Update BrowseView.tsx

--- views/BrowseView.tsx
+++ views/BrowseView.tsx
@@ -1,14 +1,20 @@
 import App from "../components/App.tsx";
 import { MainView } from "../components/MainView.tsx";
+import { Link } from "../components/Link.tsx";
 
-export function BrowseView() {
+export function BrowseView(results?: any[]) {
   return (
     <App>
       <MainView title="Browse">
-        <form action="/api/search" method="get">
+        <form action="/browse" method="get" style="padding: 4px">
             <input name="q" placeholder="Query..." id="name" type="text" />
             <input type="submit" value="Search" />
         </form>
+        <ul>
+        {results?.length && results.map(res => (
+            <li><Link href={`/manga/${res.id}`}>{res.name}</Link></li>
+        )}
+        </ul>
       </MainView>
     </App>
   )
